bc0fb233e3d99e97da30ca0bc8ef57d47dbb0d9c
Leo <mccleale@tcd.ie>
2019-10-29 19:05:41 +0000

    forgot to remove special characters

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'yeet123456'
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://authwebv2:Rocket1234@authwebv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com>/<authwebv2db>'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://authwebv2:Rocket1234@authwebv2.cxvqbz3pght8.eu-west-1.rds.amazonaws.com/authwebv2db'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
