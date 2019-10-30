7a41cfeb4d152298906f431572b446e7107a79f8
Leo <mccleale@tcd.ie>
2019-10-30 15:27:12 +0000

    css and html changes

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
 	SECRET_KEY = os.getenv('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
-	#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	if(os.getenv('ENVIRONMENT') == 0):
+		SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL')
+	else:
+		SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	WTF_CSRF_ENABLED = True
