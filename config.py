3301be6142b8d4a1c7a4cd5c15420dc4da35e397
Leo <mccleale@tcd.ie>
2019-10-30 12:50:16 +0000

    added migrations back in

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = 'ZTLA6DgGSQV0dsRc6n7Z'
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://webauthv2:Rocket1234@webauthv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com:3306/webauthv2db'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://ubuntu:@localhost:3306/db'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	WTF_CSRF_ENABLED = True
