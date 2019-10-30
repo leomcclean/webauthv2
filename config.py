79a35abbc9a26c4a09250e9e23edd26421f5eafb
Leo <mccleale@tcd.ie>
2019-10-30 11:34:58 +0000

    added secret key

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-	SECRET_KEY = os.environ.get('SECRET_KEY')
+	SECRET_KEY = 'ZTLA6DgGSQV0dsRc6n7Z'
 	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://webauthv2:Rocket1234@webauthv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com:3306/webauthv2db'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	WTF_CSRF_ENABLED = True
