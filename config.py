4cf7bbb413f751f015ba4a7f985f32753a694c89
Leo <mccleale@tcd.ie>
2019-10-30 11:17:28 +0000

    fixed typo

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'yeet123456'
+	SECRET_KEY = os.environ.get('SECRET_KEY')
 	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://webauthv2:Rocket1234@webauthv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com:3306/webauthv2db'
-	SQLALCHEMY_TRACK_MODIFICATIxONS = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	WTF_CSRF_ENABLED = True
