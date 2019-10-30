ee1a543cdaa603fe5e6fe1a0818dce9683c2e94e
Leo <mccleale@tcd.ie>
2019-10-30 10:20:38 +0000

    added port to db endpoint

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'yeet123456'
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://webauthv2:Rocket1234@webauthv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com/webauthv2db'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://webauthv2:Rocket1234@webauthv2db.cxvqbz3pght8.eu-west-1.rds.amazonaws.com:3306/webauthv2db'
 	SQLALCHEMY_TRACK_MODIFICATIxONS = False
